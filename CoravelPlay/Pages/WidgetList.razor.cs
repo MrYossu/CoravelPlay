9e8b2fb42d0d5418598bd28be9e0d9a73ac0bb2f
Avrohom Yisroel Silver <mryossu@hotmail.com>
2021-12-22 18:18:47 +0000

    Added button and event handler to add a widget using the old-fashioned approach

--- CoravelPlay/Pages/WidgetList.razor.cs
+++ CoravelPlay/Pages/WidgetList.razor.cs
@@ -11,10 +11,17 @@ namespace CoravelPlay.Pages {
     private ObservableCollection<Widget> Widgets { get; set; } = new() { new Widget { Name = "Initial" } };
     private int _n;
     public async Task HandleAsync(NewWidgetCreated broadcasted) {
+      int hash = GetHashCode();
       Widget w = broadcasted.Widget;
       Widgets.Add(w);
       _n++;
       await Task.CompletedTask;
+    }
+
+    public void AddNewWidget(Widget w) {
+      int hash = GetHashCode();
+      Widgets.Add(w);
+      StateHasChanged();
     }
   }
 }
